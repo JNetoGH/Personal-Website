e9c39f9476d4bb6c6a1ee5904c0316a3ef713905
JNeto <joaomendes15@gmail.com>
2026-03-09 18:18:42 +0000

    Test

--- src/GameDeveloperPortfolio.tsx
+++ src/GameDeveloperPortfolio.tsx
@@ -24,7 +24,7 @@ type ProfileSuffix = {
 };
 const PROFILE_TYPING_SUFFIXES: ProfileSuffix[] = [
   { text: "s...", highlight: false },
-  { text: " experiences", highlight: true }
+  { text: " stuff", highlight: true }
 ];
 const PROFILE_TYPING_SPEED_MS = 60;
 const PROFILE_BACKSPACE_SPEED_MS = 30;
